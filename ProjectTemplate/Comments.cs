d0adf5108896adc67a0739e6e8c0b42b38c2b364
mkhickco <mkhickco@asu.edu>
2022-07-31 10:16:17 -0700

    updated managerfeed page and added comment functionality

---
+++ ProjectTemplate/Comments.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectTemplate
+{
+    public class Comments
+    {
+        public int CommentID;
+        public int UserID;
+        public string FirstName;
+        public string LastName;
+        public int PostID;
+        public string Comment;
+        public string DateTime;
+    }
+}
